d3d9485a7cfcafea9152edba97d05db658fa7d2f
Nastya <anastasic.curly@mail.ru>
2022-10-14 20:46:10 +0300

    Add task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write ("Введите 1-е число: "); 
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите 2-е число: "); 
+int b = Convert.ToInt32(Console.ReadLine());
+int result =a;
+for (int i = 1; i<b; i++)
+{
+result=result*a;
+}
+Console.WriteLine (result + " ");
