afaf4e1d439c1c3c6c9c864013491bb3a359a100
Nastya <anastasic.curly@mail.ru>
2022-10-14 20:49:42 +0300

    Add task27

---
+++ task27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите  число: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+int sum =0;
+while (n > 0)
+{
+ int m=n%10;
+sum= sum +m;
+ n=n/10;
+}
+Console.WriteLine (sum + " ");
+
